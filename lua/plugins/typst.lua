926a3ac0108a0f69be6ef8f0aac54a4809814a0b
Matteo Bongiovanni <MatteoBongiovanni@proton.me>
2026-04-15 13:09:51 +0200

    typst

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -2,7 +2,7 @@ return {
   'chomosuke/typst-preview.nvim',
   lazy = false, -- or ft = 'typst'
   version = '1.*',
-  build = function() require 'typst-preview'.update() end,
+  -- build = function() require 'typst-preview'.update() end,
   keys = {
     { "<leader>tp", "<cmd>TypstPreview<cr>" }
   }
